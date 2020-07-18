67b9f993337b61f86629c5db9aa419d41cadadd4
confident-architect-8379 <s.kanagui@gmail.com>
2020-07-18 12:00:24 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -9,7 +9,7 @@ describe "say_hello" do
 
   it 'defaults to Ruby Programmer when no name is passed in' do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-    say_hello(name)
+    say_hello()
   end
 end
 def say_hello (name = "Ruby Programmer!")
